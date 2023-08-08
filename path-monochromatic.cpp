c21d17aa4c351982faf196e04f1b2a9a7aa5012e
raphaelyuster <raphael.yuster@gmail.com>
2023-08-08 18:13:48 +0300

    Update path-monochromatic.cpp

--- path-monochromatic.cpp
+++ path-monochromatic.cpp
@@ -1,15 +1,14 @@
 /*
 * This program computes the value s(Q) for all tournaments Q of order q where s(Q) is define in the paper "Path-monochromatic bounded depth rooted trees in (random) tournaments"
 * Recalling the definition: For a tournament Q on q vertices, for a 2-edge coloring c of it, and for a vertex v, let s(Q,c,v) denote the number of vertices (other than v) that are monochromatically
-* dominated by v using paths of length at most 2. Let s(Q, c) be the sum of s(Q, c, v) taken over all vertice (As Q is a tournament, s(Q,c) can also be determined by the number of directed triangles
-* and the number of monochromatic directed triangles in Q), and let s(Q) be the minimum of s(Q, c) taken over all colorings.
+* dominated by v using paths of length at most 2. Let s(Q, c) be the sum of s(Q, c, v) taken over all vertices and let s(Q) be the minimum of s(Q, c) taken over all colorings.
 *
 * Recall that our goal is to find some tournament for which s(Q)/q(q-1) > 2/3.
 */
 
 /* Prerequisites:
 *
-* 1) A file containing the databse of all tournaments on q vertices. This is taken from https://users.cecs.anu.edu.au/~bdm/data/digraphs.html for q <= 9.
+* A file containing the databse of all tournaments on q vertices. This is taken from https://users.cecs.anu.edu.au/~bdm/data/digraphs.html for q <= 9.
 *
 */
 
@@ -22,7 +21,7 @@ const int C = 2;      // number of colors; we will only use two colors
 const int numTournaments[] = { 1,1,1,2,4,12,56,456,6880,191536 };       // number of tournaments on q vertices for q = 0,...,9.  
 const int directedTrainglesFilter = 30;									// to speed up the run, only check tournaments with al least this amount of directed triangles.
 																		// To check all tournaments, set value to 0
-const int resultFilter = 2 * q * (q - 1) / 3;									// discard tournaments which have a coloring that does not reach as lest this result
+const int resultFilter = 2 * q * (q - 1) / 3;							// discard tournaments which have a coloring that does not reach as least this result
 
 int tournamentDB[191536][q][q];											// any array containing the adjacency matrices of all tournaments of order q
 
@@ -49,8 +48,8 @@ void nextTournamentFromFile(int r)
 }
 
 void setTournamentDB() {
-	for (int counter = 0; counter < numTournaments[q]; counter++)  // loop on all tournaments in the tournamnt database.
-		nextTournamentFromFile(counter);  // read the next tournament Q from the tournament database file.
+	for (int counter = 0; counter < numTournaments[q]; counter++)	// loop on all tournaments in the tournamnt database.
+		nextTournamentFromFile(counter);							// read the next tournament from the tournament database file.
 }
 
 int directedTrianglesDB[q * (q - 1) * (q - 2) / 6][3];
@@ -116,7 +115,7 @@ int countDepth2Reachability(int r) {
 int nextColor(int r) {
 	for (int i = 0; i < q; i++)
 		for (int j = i + 1; j < q; j++) {
-			if (i + j == 1) // this saves half the runtime as the first examined edge can always be fixed some particular color, say color 1.
+			if (i + j == 1) // this saves half the runtime as the first examined edge can always be fixed some particular color, say color 1. This may be discarded at the expense of runtime
 				continue;
 			int currentColor = abs(tournamentDB[r][i][j]);
 			if (currentColor < C) {
